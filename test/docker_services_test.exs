ca35e28418861ed8b2bee1bac556eb58d8ae4cd8
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-17 22:09:58 +0200

    Test help text

--- test/docker_services_test.exs
+++ test/docker_services_test.exs
@@ -1,13 +1,32 @@
 defmodule DockerServicesTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest DockerServices
 
-  test "bootstrap generates a shell file" do
+  test "'bootstrap' generates a shell file" do
     File.rm("tmp/shell")
 
     DockerServices.CLI.main([ "bootstrap" ])
 
     { :ok, content } = File.read("tmp/shell")
     assert content =~ "function docker_services()"
+  end
+
+  test "'help' shows help text" do
+    output = capture_io fn ->
+      DockerServices.CLI.main([ "help" ])
+    end
+
+    assert output =~ "Usage:"
+  end
+
+  test "unknown commands shows that they are unknown and also shows the help text" do
+    output = capture_io fn ->
+      DockerServices.CLI.main([ "unknown1" ])
+    end
+
+    assert output =~ "Unknown command"
+    assert output =~ "unknown1"
+    assert output =~ "Usage:"
   end
 end
