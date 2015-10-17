33ae8c5411997333425d4d4f3039df52ba55a066
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-17 21:32:38 +0200

    Extract init from cli.
    Keeps the CLI focused on just the commands.

--- lib/docker_services/cli.ex
+++ lib/docker_services/cli.ex
@@ -1,40 +1,9 @@
 defmodule DockerServices.CLI do
   def main([ "init" ]) do
-    create_home_directory
-    write_shell_script
+    DockerServices.Init.run
   end
 
   def main(args) do
     IO.inspect "Unknown args: #{args}"
-  end
-
-  defp create_home_directory do
-    shell_file_path
-    |> Path.dirname
-    |> File.mkdir_p
-  end
-
-  defp write_shell_script do
-    File.write shell_file_path, """
-    function docker_services() {
-      if [ "$1" == "set_environment_variables" ]; then
-        # docker_services will set envs here later without running any elixir code
-        # as that is a bit too slow to do while navigating the filesystem
-        echo "" > /dev/null
-      else
-        /usr/local/bin/docker_services $@
-      fi
-
-      # Reload this script after init has been run since it might have changed
-      if [ "$1" == "init" ]; then
-        source "#{shell_file_path}"
-      fi
-    }
-    """
-  end
-
-  defp shell_file_path do
-    Application.get_env(:docker_services, :shell_file_path)
-    |> String.replace("HOME_PATH", System.get_env("HOME"))
   end
 end
