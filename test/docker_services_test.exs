1c32437ac4c7e3c7741f34020e8d1a3f6314b193
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-17 22:57:23 +0200

    Wip test for start

--- test/docker_services_test.exs
+++ test/docker_services_test.exs
@@ -16,6 +16,35 @@ defmodule DockerServicesTest do
     assert content =~ "function docker_services()"
   end
 
+  #test "'start' starts the services specified in config" do
+  #  # WIP
+
+  #  File.rm_rf("tmp/test_project")
+  #  File.mkdir_p("tmp/test_project")
+  #  File.cd("tmp/test_project")
+  #  File.write("dev.yml", """
+  #  docker_services:
+  #    redis:
+  #      image: redis:2.8
+  #  """)
+
+  #  # docker = Application.get_env(:docker_services, :docker_client)
+  #  #{:ok, external_port} = docker.start(name: name, image_name: image_name)
+  #  # write port to file, etc.
+  #  #:ok = docker.stop(name)
+
+  #  output = capture_io fn ->
+  #    DockerServices.CLI.main([ "start" ])
+  #  end
+
+  #  { :ok, content } = File.read("~/.docker_services/something/env.load")
+  #  assert content == "export REDIS_PORT=5555"
+
+  #  # test that we store env before and after we changed it
+  #  # source unload
+  #  # source load
+  #end
+
   test "'help' shows help text" do
     output = capture_io fn ->
       DockerServices.CLI.main([ "help" ])
