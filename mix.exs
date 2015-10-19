b2bfc26b5e79df62e52b030aef37cd2e3bcac132
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-19 23:18:46 +0200

    Up version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DockerServices.Mixfile do
 
   def project do
     [app: :docker_services,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
