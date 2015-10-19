682f61a3e8ef5faf42a79d4782a5e2528b3d51ac
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-19 18:53:52 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DockerServices.Mixfile do
 
   def project do
     [app: :docker_services,
-     version: "0.0.1",
+     version: "0.0.3",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
