6324ceb2681b982c0043e9b36e9100ac4cc76771
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-22 19:28:05 +0200

    Fix version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DockerServices.Mixfile do
 
   def project do
     [app: :docker_services,
-     version: "0.9.1",
+     version: "0.9.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
