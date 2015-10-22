abb04bf17ba822d9cdfc8c9d478d37d7dc016fca
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-22 19:10:31 +0200

    Better file names

--- script/ci/run_tests_and_build_release.sh
+++ script/ci/run_tests_and_build_release.sh
@@ -6,6 +6,6 @@ export PATH="$HOME/dependencies/erlang/bin:$HOME/dependencies/elixir/bin:$PATH"
 set -e
 
 mix test
-MIX_ENV=prod mix escript.build
 
+MIX_ENV=prod mix escript.build
 mv docker_services $CIRCLE_ARTIFACTS/
