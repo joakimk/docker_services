d3deb39af0b92db8f8792ced8b4aebd4daa11df1
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-22 18:57:39 +0200

    Add release build to CI run

--- script/ci/test.sh
+++ script/ci/test.sh
@@ -3,4 +3,9 @@
 export MIX_ENV="test"
 export PATH="$HOME/dependencies/erlang/bin:$HOME/dependencies/elixir/bin:$PATH"
 
+set -e
+
 mix test
+MIX_ENV=prod mix escript.build
+
+mv docker_services $CIRCLE_ARTIFACTS/
