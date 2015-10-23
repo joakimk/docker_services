c72925485bdf036ef5f00e268fb55812ae5b2bfa
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-23 22:13:20 +0200

    Refactor parsing code

--- lib/docker_services/docker_images.ex
+++ lib/docker_services/docker_images.ex
@@ -2,9 +2,22 @@ defmodule DockerServices.DockerImages do
   def parse(output) do
     output
     |> String.split("\n")
-    |> Enum.filter(fn line -> !String.contains?(line,"TAG") end)
-    |> Enum.map(fn line -> String.split(line) |> Enum.take(2) end)
-    |> Enum.filter(fn parts -> parts != [] end)
-    |> Enum.map(fn [name, version] -> "#{name}:#{version}" end)
+    |> Enum.map(&parse_line/1)
+    |> Enum.reject(&ignore?/1)
+    |> Enum.map(&name_and_version/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split
+    |> Enum.take(2)
+  end
+
+  defp ignore?([ "REPOSITORY", _ ]), do: true
+  defp ignore?([]), do: true
+  defp ignore?(_),  do: false
+
+  def name_and_version([name, version]) do
+    "#{name}:#{version}"
   end
 end
