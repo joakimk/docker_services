ff4bcfe85fbed78739f633c4bafaf5a144d30dd8
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-10-23 21:18:37 +0200

    Cleanup

--- lib/docker_services/cli.ex
+++ lib/docker_services/cli.ex
@@ -1,14 +1,13 @@
 defmodule DockerServices.CLI do
-  def main([ single_argument ]), do: command(single_argument |> String.to_atom)
-  def main([ "backup", service, archive_path ]), do: DockerServices.Backup.backup(service, archive_path)
-  def main([ "restore", service, archive_path ]), do: DockerServices.Backup.restore(service, archive_path)
+  def main([ first_argument | rest ]), do: command(first_argument |> String.to_atom, rest)
+  def command(name, []), do: command(name)
 
+  def command(:backup,  [ service, archive_path ]), do: DockerServices.Backup.backup(service, archive_path)
+  def command(:restore, [ service, archive_path ]), do: DockerServices.Backup.restore(service, archive_path)
   def command(:bootstrap), do: DockerServices.Bootstrap.run
   def command(:help),      do: DockerServices.Help.show
   def command(:start),     do: DockerServices.Runner.start
   def command(:stop),      do: DockerServices.Runner.stop
-
-  def main([]), do: command("")
 
   def command(other) do
     IO.puts "Unknown command: #{other}\n"
